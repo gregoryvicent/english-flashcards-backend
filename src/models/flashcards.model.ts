fa831eae3b39c55de089eee361085b324faa6fb4
Gregory Vicent <gregoryvicent.dev@gmail.com>
2022-12-03 08:32:14 -0400

    Story 008: Added comments to model Flashcards

--- src/models/flashcards.model.ts
+++ src/models/flashcards.model.ts
@@ -1,6 +1,8 @@
+// Model for the Flashcards table
 import { DataBaseConnection } from '../classes/dataBaseConnection.class';
 import { DataTypes } from 'sequelize';
 
+// "FlashCardModel" inherits from "DataBaseConnection" to use the same database connection and It can use the property "this.sequelize".
 class FlashCardModel extends DataBaseConnection {
   public createModel() {
     const Flashcard = this.sequelize.define('Flashcard', {
@@ -22,6 +24,7 @@ class FlashCardModel extends DataBaseConnection {
   }
 }
 
+// Here the class is instantiated to only export the "createModel()" method that contains what is necessary to work with this Flashcards model.
 const flashcard = new FlashCardModel();
 const Flashcard = flashcard.createModel();
 
